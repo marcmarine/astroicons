e6b772c575a838fe03d7a3fae63ba5920c195ae1
Marc Mariné <shenobi@gmail.com>
2026-07-05 13:18:05 +0200

    Add Tailwind CSS support

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,8 +1,9 @@
 import react from "@vitejs/plugin-react";
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), tailwindcss()],
 	base: process.env.PAGES_BASE_PATH,
 });
