1a59e6142073edda303a7434ee864f69a995b906
Marc Mariné <shenobi@gmail.com>
2026-07-09 22:40:46 +0200

    Add Figtree 700 font weight

--- docs/src/main.tsx
+++ docs/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "@fontsource/eb-garamond/latin-600.css";
 import "@fontsource/figtree/latin-500.css";
+import "@fontsource/figtree/latin-700.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
