986d83a902195baa41cab63d40625da98f402611
Marc Mariné <shenobi@gmail.com>
2026-07-10 19:15:42 +0200

    Remove base public path from Vite config

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,9 +1,7 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
-	base: process.env.PAGES_BASE_PATH,
 });
