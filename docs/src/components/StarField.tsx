4575a0ba1463936e1cf01b23ce1bd1bc72ff01be
Marc Mariné <shenobi@gmail.com>
2026-07-22 20:16:54 +0200

    Add spring animation to star field

--- docs/src/components/StarField.tsx
+++ docs/src/components/StarField.tsx
@@ -1,8 +1,13 @@
-import { motion, useScroll, useTransform } from "motion/react";
+import { motion, useScroll, useSpring, useTransform } from "motion/react";
 
 export default function StarField() {
 	const { scrollYProgress } = useScroll();
-	const y = useTransform(scrollYProgress, [0, 1], [0, -200]);
+
+	const smoothProgress = useSpring(scrollYProgress, {
+		stiffness: 20,
+	});
+
+	const y = useTransform(smoothProgress, [0, 1], [0, -120]);
 
 	return <motion.div className="star-field" style={{ y }} />;
 }
