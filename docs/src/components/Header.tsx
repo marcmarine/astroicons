20db4d235dc67938de8fca87804e977be56e8f11
Marc Mariné <shenobi@gmail.com>
2026-08-13 21:23:53 +0200

    docs: Add Sun and Moon icons to header logo

--- docs/src/components/Header.tsx
+++ docs/src/components/Header.tsx
@@ -1,11 +1,18 @@
-import { REPO_URL } from "../lib/constants";
+import { Sun, Moon } from '@astroicons/react'
 import ThemeToggle from "./ThemeToggle";
+import { REPO_URL } from "../lib/constants";
 
 export default function Header() {
 	return (
 		<header className="px-4 py-3 flex items-center justify-between border-b border-b-(--border-color)">
 			<div className="flex items-center">
+			<div className="flex items-center gap-1">
+				<div className="flex items-center">
+					<Sun strokeWidth={1.8} size={20} stroke="cyan" />
+					<Moon strokeWidth={1.8} size={20} className="-ml-2" stroke="gold" />
+				</div>
 				<h1 className="text-xl font-bold">Astroicons</h1>
+			</div>
 			</div>
 			<div className="flex gap-2">
 				<ThemeToggle />
